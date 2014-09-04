111695d35231bf8e4b8aa2a7c1ecd91cb2e9d335
Viv Richards <vivrichards600@hotmail.com>
2014-09-04 20:43:41 +0100

    solution cleaned up, it wasnt meeting all kata criteria either returning invalid for an incorrect number! Tests altered to pass using new code.

--- vb/phone-numbers-kata/phone-numbers-kata/PhoneNumberTests.vb
+++ vb/phone-numbers-kata/phone-numbers-kata/PhoneNumberTests.vb
@@ -4,60 +4,53 @@
 Public Class PhoneNumberTests
 
     <TestMethod()>
-    <ExpectedException(GetType(System.Exception))> _
-    Public Sub Phone_Number_Must_Be_More_Than_1_Digit()
-        Validate.PhoneNumber("1")
+    Public Sub Phone_Number_Must_Be_More_Than_1_Digit_In_Length()
+        Assert.IsTrue(Validate.PhoneNumber("1") = INVALID_ERROR)
     End Sub
 
     <TestMethod()>
-      <ExpectedException(GetType(System.Exception))> _
-    Public Sub Phone_Number_Must_Be_More_Than_10_Digits()
-        Validate.PhoneNumber("9123456789")
+    Public Sub Phone_Number_Must_Be_More_Than_10_Digits_In_Length()
+        Assert.IsTrue(Validate.PhoneNumber("9123456789") = INVALID_ERROR)
     End Sub
 
     <TestMethod()>
-    <ExpectedException(GetType(System.Exception))> _
-    Public Sub Phone_Number_Must_Be_Less_Than_12_Digits()
-        Validate.PhoneNumber("912345678909")
+    Public Sub Phone_Number_Must_Be_Less_Than_12_Digits_In_Length()
+        Assert.IsTrue(Validate.PhoneNumber("912345678909") = INVALID_ERROR)
     End Sub
 
     <TestMethod()>
-    Public Sub Phone_Number_Must_Be_11_Digits()
+    Public Sub Phone_Number_Must_Be_11_Digits_In_Length()
         Assert.IsTrue(Validate.PhoneNumber("92345567871") = VALID_TELEPHONE_NUMBER)
     End Sub
 
     <TestMethod()>
     Public Sub Phone_Number_Must_Begin_With_9()
-        Assert.IsTrue(Validate.PhoneNumber("92345567872") = VALID_TELEPHONE_NUMBER)
+        Assert.IsTrue(Validate.PhoneNumber("92345567871") = VALID_TELEPHONE_NUMBER)
     End Sub
 
     <TestMethod()>
-    <ExpectedException(GetType(System.Exception))> _
     Public Sub Fail_When_Phone_Number_Does_Not_Begin_With_9()
-        Validate.PhoneNumber(INVALID_TELEPHONE_NUMBER)
+        Assert.IsTrue(Validate.PhoneNumber(INVALID_TELEPHONE_NUMBER) = INVALID_ERROR)
     End Sub
 
     <TestMethod()>
-    Public Sub Phone_Number_Must_Remove_Spaces()
+    Public Sub Phone_Number_Ignores_Spaces()
         Assert.IsTrue(Validate.PhoneNumber("923 4556 787 1") = VALID_TELEPHONE_NUMBER)
     End Sub
 
     <TestMethod()>
-    Public Sub Phone_Number_Must_Remove_Special_Characters()
+    Public Sub Phone_Number_Ignores_Special_Characters()
         Assert.IsTrue(Validate.PhoneNumber("9-2(345)567,871") = VALID_TELEPHONE_NUMBER)
     End Sub
 
     <TestMethod()>
-    <ExpectedException(GetType(System.Exception))> _
-    Public Sub Fail_When_Phone_Number_Contains_Alpha_Characters()
-        Validate.PhoneNumber("9123456789A")
+    Public Sub Invalid_Error_When_Phone_Number_Contains_Alpha_Characters()
+        Assert.IsTrue(Validate.PhoneNumber("9123456789A") = INVALID_ERROR)
     End Sub
 
-    Private ExpectedResult = String.Empty
-    Private ActualResult = String.Empty
     Private VALID_TELEPHONE_NUMBER = "92345567871"
     Private INVALID_TELEPHONE_NUMBER = "82345567871"
-    Private TELEPHONE_NUMBER = String.Empty
+    Private INVALID_ERROR = "invalid"
 
 End Class
 
--- vb/phone-numbers-kata/phone-numbers-kata/Validate.vb
+++ vb/phone-numbers-kata/phone-numbers-kata/Validate.vb
@@ -2,14 +2,12 @@
 
     Public Shared Function PhoneNumber(telephoneNumber As String) As String
         Dim numberToCheck = telephoneNumber
-
         numberToCheck = numberToCheck.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "").Replace(",", "")
 
-        If Not Left(numberToCheck, 1) = "9" Then Throw New Exception("Phone number must start with a 9")
-        If Not numberToCheck.Length > 1 Then Throw New Exception("Phone number must be greater than 1 numbers")
-        If Not numberToCheck.Length > 10 Then Throw New Exception("Phone number must be greater than 10 numbers")
-        If Not numberToCheck.Length < 12 Then Throw New Exception("Phone number must be less than 12 numbers")
-        If Not IsNumeric(numberToCheck) Then Throw New Exception("Phone number must not contain alpha characters")
+        If Not numberToCheck.StartsWith("9") = True Then Return "invalid"
+        If Not numberToCheck.Length > 10 Then Return "invalid"
+        If Not numberToCheck.Length < 12 Then Return "invalid"
+        If Not IsNumeric(numberToCheck) Then Return "invalid"
 
         Return numberToCheck
 
